9b7e9eefe30d43ea2f108d96309caf49621c2284
jin <1291794836@qq.com>
2019-12-28 17:31:54 +0800

    添加项目文件。

---
+++ PaintView.cpp
@@ -0,0 +1,97 @@
+﻿
+// PaintView.cpp: CPaintView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Paint.h"
+#endif
+
+#include "PaintDoc.h"
+#include "PaintView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPaintView
+
+IMPLEMENT_DYNCREATE(CPaintView, CView)
+
+BEGIN_MESSAGE_MAP(CPaintView, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CPaintView 构造/析构
+
+CPaintView::CPaintView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CPaintView::~CPaintView()
+{
+}
+
+BOOL CPaintView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CPaintView 绘图
+
+void CPaintView::OnDraw(CDC* /*pDC*/)
+{
+	CPaintDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+void CPaintView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CPaintView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CPaintView 诊断
+
+#ifdef _DEBUG
+void CPaintView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CPaintView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CPaintDoc* CPaintView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CPaintDoc)));
+	return (CPaintDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CPaintView 消息处理程序
